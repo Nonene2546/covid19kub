909e0f377f87985fed643b7221f941d5589ce021
Chaksawat <88503339+Nonene2546@users.noreply.github.com>
2021-10-26 19:57:26 +0700

    Update RB.js

--- RB.js
+++ RB.js
@@ -1,6 +1,6 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
+// let searchBtn = document.querySelector(".bx-search");
 
 closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
